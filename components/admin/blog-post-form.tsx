442980675963eb4d2a4c2d1ee84106fef22d5db0
Harshit Singh <jabincreators@gmail.com>
2025-07-28 16:21:10 +0530

    Fixed Blogs

--- components/admin/blog-post-form.tsx
+++ components/admin/blog-post-form.tsx
@@ -9,7 +9,6 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { Checkbox } from '@/components/ui/checkbox'
-import { Separator } from '@/components/ui/separator'
 import { Label } from '@/components/ui/label'
 import {
   Select,
